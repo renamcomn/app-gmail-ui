af714c6e12705dc061d319b272724d1b694684c8
Renan Henrique <renamcomn@Renans-Mac-mini.local>
2024-03-05 10:20:46 -0300

    boilerplate with tabs

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Home() {
+    return (
+    <View className='flex-1 bg-gray-900 items-center justify-center'>
+        <Text className='text-white'>Home</Text>
+    </View>
+    )
+}
